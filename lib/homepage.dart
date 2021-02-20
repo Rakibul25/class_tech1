ce17595aba029afb0ddd86ff6e55efdd22e3d7b7
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-21 02:50:45 +0600

    way 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
-class _HomePageState extends State<HomePage> {
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Center(
+//             // child: Card(
+//             //   elevation: 12,
+//             //   shape: RoundedRectangleBorder(
+//             //     borderRadius: BorderRadius.circular(24),
+//             //   ),
+//             //   child: Padding(
+//             //     padding: EdgeInsets.all(32),
+//             //     child: Column(
+//             //       mainAxisSize: MainAxisSize.min,
+//             //       //mainAxisAlignment: MainAxisAlignment.center,
+//             //       //crossAxisAlignment: CrossAxisAlignment.stretch,
+
+//             //       children: <Widget>[
+//             //         //Container(color: Colors.blue, child: Text("Hello User")),
+//             //         Icon(Icons.ac_unit),
+//             //         // FlutterLogo(size: 48),
+//             //         SizedBox(
+//             //           height: 10,
+//             //         ),
+//             //         Column(
+//             //           crossAxisAlignment: CrossAxisAlignment.start,
+//             //           children: <Widget>[
+//             //             Text(
+//             //               'Rakib',
+//             //               style: Theme.of(context).textTheme.headline5,
+//             //             ),
+//             //             Text('Description.....',
+//             //                 style: Theme.of(context).textTheme.headline6),
+//             //           ],
+//             //         ),
+//             //       ],
+//             //     ),
+//             //   ),
+//             // ),
+//             ),
+//       ),
+//     );
+//   }
+// }
+
+class RowDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var scaffold = Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: SafeArea(
         child: Center(
@@ -54,58 +100,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-    return scaffold;
   }
 }
-
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.blueAccent,
-//       body: SafeArea(
-//         child: Center(
-//           child: Card(
-//             margin: EdgeInsets.all(16),
-//             elevation: 12,
-//             shape: RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(24),
-//             ),
-//             color: Colors.white,
-//             child: Padding(
-//               padding:
-//                   const EdgeInsets.symmetric(horizontal: 24.0, vertical: 36),
-//               child: Row(
-//                 children: [
-//                   FlutterLogo(size: 50),
-//                   SizedBox(
-//                     width: 16,
-//                   ),
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: <Widget>[
-//                       Text(
-//                         'Title of the Card',
-//                         style: Theme.of(context).textTheme.headline5,
-//                       ),
-//                       Text('Some description',
-//                           style: Theme.of(context).textTheme.headline6),
-//                     ],
-//                   ),
-//                   SizedBox(
-//                     width: 20,
-//                   ),
-//                   Icon(
-//                     Icons.navigate_next,
-//                     size: 36,
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+//import 'package:class_tech/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:class_tech/HomePage.dart';
 
 import 'HomePage.dart';
 
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: RowDemo(),
     );
   }
 }
