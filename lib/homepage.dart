ab127c4f3325d99ba9f575ec19495c0fdd42d28b
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-21 02:30:06 +0600

    column inside row

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,53 +8,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Card(
-            elevation: 12,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(32),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                //mainAxisAlignment: MainAxisAlignment.center,
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-
-                children: <Widget>[
-                  //Container(color: Colors.blue, child: Text("Hello User")),
-                  Icon(Icons.ac_unit),
-                  // FlutterLogo(size: 48),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Title',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      Text('Description.....',
-                          style: Theme.of(context).textTheme.headline6),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RowDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    var scaffold = Scaffold(
       backgroundColor: Colors.blueAccent,
       body: SafeArea(
         child: Center(
@@ -100,5 +54,58 @@ class RowDemo extends StatelessWidget {
         ),
       ),
     );
+    return scaffold;
   }
 }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.blueAccent,
+//       body: SafeArea(
+//         child: Center(
+//           child: Card(
+//             margin: EdgeInsets.all(16),
+//             elevation: 12,
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(24),
+//             ),
+//             color: Colors.white,
+//             child: Padding(
+//               padding:
+//                   const EdgeInsets.symmetric(horizontal: 24.0, vertical: 36),
+//               child: Row(
+//                 children: [
+//                   FlutterLogo(size: 50),
+//                   SizedBox(
+//                     width: 16,
+//                   ),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: <Widget>[
+//                       Text(
+//                         'Title of the Card',
+//                         style: Theme.of(context).textTheme.headline5,
+//                       ),
+//                       Text('Some description',
+//                           style: Theme.of(context).textTheme.headline6),
+//                     ],
+//                   ),
+//                   SizedBox(
+//                     width: 20,
+//                   ),
+//                   Icon(
+//                     Icons.navigate_next,
+//                     size: 36,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
