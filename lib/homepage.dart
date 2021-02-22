0578f65704a344de77cc3ece2e485465282e4456
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-23 01:15:51 +0600

    practicing row column

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,27 +8,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // Widget cir = new Container(
-    //   width: 20.0,
-    //   height: 20.0,
-    //   decoration: new BoxDecoration(
-    //     color: Colors.orange,
-    //     shape: BoxShape.circle,
-    //   ),
-    // );
     return Scaffold(
       body: SafeArea(
-        child: Card(
-          margin: EdgeInsets.all(16),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-          color: Colors.lightGreen,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15),
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(12.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                recir(),
-                recir(),
+                Text(
+                  "Flutter",
+                  style: TextStyle(fontSize: 30),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: Text(
+                    "Flutter is an open-source UI software development kit created by Google. It is used to develop applications for Android, iOS, Linux, Mac, Windows, Google Fuchsia, and the web from a single codebase. The first version of Flutter was known as codename and ran on the Android operating system.",
+                    style: TextStyle(
+                        //backgroundColor: Colors.blue[100],
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.all(15),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: Colors.blue[200],
+                  child: Row(
+                    children: [
+                      starshape(),
+                      starshape(),
+                      starshape(),
+                      starshape(),
+                    ],
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.all(15),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: Colors.blue[200],
+                  child: Row(children: [
+                    likeshape(),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Icon(
+                      Icons.thumb_down,
+                      color: Colors.red,
+                    )
+                  ]),
+                ),
               ],
             ),
           ),
@@ -38,16 +71,22 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Widget recir() {
+Widget starshape() {
   return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Container(
-      width: 30.0,
-      height: 35.0,
-      decoration: new BoxDecoration(
-        color: Colors.orange,
-        shape: BoxShape.circle,
-      ),
+    padding: const EdgeInsets.all(10.0),
+    child: Icon(
+      Icons.star,
+      color: Colors.red,
+    ),
+  );
+}
+
+Widget likeshape() {
+  return Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: Icon(
+      Icons.thumb_up,
+      color: Colors.red,
     ),
   );
 }
