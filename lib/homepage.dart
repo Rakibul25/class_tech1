1e76b130e9eff3d5e390d33f62537520adf0c1a4
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-02-23 10:47:24 +0600

    basic listview

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,75 +10,46 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Flutter",
-                  style: TextStyle(fontSize: 30),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8, bottom: 8),
-                  child: Text(
-                    "Flutter is an open-source UI software development kit created by Google. It is used to develop applications for Android, iOS, Linux, Mac, Windows, Google Fuchsia, and the web from a single codebase. The first version of Flutter was known as codename and ran on the Android operating system.",
-                    style: TextStyle(
-                        //backgroundColor: Colors.blue[100],
-                        fontSize: 20,
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.all(15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  color: Colors.blue[200],
-                  child: Row(
-                    children: [
-                      starshape(),
-                      starshape(),
-                      starshape(),
-                      starshape(),
-                    ],
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.all(15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  color: Colors.blue[200],
-                  child: Row(children: [
-                    likeshape(),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Icon(
-                      Icons.thumb_down,
-                      color: Colors.red,
-                    )
-                  ]),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+          child: ListView(
+        children: [
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+          card1(),
+        ],
+      )),
     );
   }
 }
 
-Widget starshape() {
-  return Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: Icon(
-      Icons.star,
-      color: Colors.red,
+Widget card1() {
+  return Container(
+    height: 100,
+    child: Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      color: Colors.lightGreen,
+      child: Text(
+        'Hello everyone',
+        //textAlign: TextAlign.center,
+        textAlign: TextAlign.center,
+      ),
     ),
   );
+  // return Container(
+  //   child Card(
+  //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+  //   color: Colors.lightGreen,
+  //   child: Text('Hello everyone'),
+  // ),
+  //);
 }
 
 Widget likeshape() {
