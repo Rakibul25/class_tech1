4e387b39c9b90f6eb6b4abf335f0c1deccd9d481
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-03-05 15:51:41 +0600

    getx done

--- lib/shoppingpage.dart
+++ lib/shoppingpage.dart
@@ -1,3 +1,4 @@
+import 'package:class_tech/controller/cartcontroller.dart';
 import 'package:class_tech/controller/shoppingcontroller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,8 @@ import 'package:get/get.dart';
 
 class ShoppingPage extends StatelessWidget {
   final shoppingcontroller = Get.put(ShoppingController());
+  final cartController = Get.put(CartController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,24 +48,24 @@ class ShoppingPage extends StatelessWidget {
                               ),
                               RaisedButton(
                                 onPressed: () {
-                                  // cartController
-                                  //     .addToCart(controller.products[index]);
+                                  cartController
+                                      .addToCart(controller.products[index]);
                                 },
                                 color: Colors.blue,
                                 textColor: Colors.white,
                                 child: Text('Add to Cart'),
                               ),
-                              Obx(() => IconButton(
-                                    icon: controller
-                                            .products[index].isFavorite.value
-                                        ? Icon(Icons.check_box_rounded)
-                                        : Icon(Icons
-                                            .check_box_outline_blank_outlined),
-                                    onPressed: () {
-                                      controller.products[index].isFavorite
-                                          .toggle();
-                                    },
-                                  ))
+                              // Obx(() => IconButton(
+                              //       icon: controller
+                              //               .products[index].isFavorite.value
+                              //           ? Icon(Icons.check_box_rounded)
+                              //           : Icon(Icons
+                              //               .check_box_outline_blank_outlined),
+                              //       onPressed: () {
+                              //         controller.products[index].isFavorite
+                              //             .toggle();
+                              //       },
+                              //     ))
                             ],
                           ),
                         ),
@@ -70,10 +73,29 @@ class ShoppingPage extends StatelessWidget {
                     });
               }),
             ),
-            Text("Total amount : "),
+            GetX<CartController>(builder: (controller) {
+              return Text(
+                "Total amount : \$ ${controller.totalPrice}",
+                style: TextStyle(fontSize: 25),
+              );
+            }),
             SizedBox(height: 100)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        icon: Icon(
+          Icons.add_shopping_cart_rounded,
+          color: Colors.black,
+        ),
+        label: GetX<CartController>(builder: (controller) {
+          return Text(
+            controller.count.toString(),
+            style: TextStyle(color: Colors.black, fontSize: 24),
+          );
+        }),
       ),
     );
   }
